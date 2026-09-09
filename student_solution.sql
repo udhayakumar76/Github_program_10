2430ace59f1984a97f1d7c6da9474ce22d992d98
R.udhayakumar <udhaya2775@gmail.com>
2026-09-09 09:36:07 +0530

    Update student_solution.sql

--- student_solution.sql
+++ student_solution.sql
@@ -1,1 +1,32 @@
+CREATE TABLE Course(
+CourseID INT,
+CourseName VARCHAR(30),
+Credits INT
+);
+INSERT INTO Course VALUES
+(201,'Database Systems',4),
+(202,'Data Structures',3),
+(203,'Mathematics',4);
+CREATE TABLE Enrollment(
+EnrollmentID INT,
+StudentID INT,
+CourseID INT
+);
+INSERT INTO Enrollment VALUES
+(1,1001,201),
+(2,1001,202),
+(3,1002,203),
+(4,1003,201);
+SELECT Course.CourseID,
+Course.CourseName,
+Enrollment.StudentID
+FROM Course
+LEFT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
+SELECT Course.CourseID,
+Course.CourseName,
+Enrollment.StudentID
+FROM Course
+RIGHT JOIN Enrollment
+ON Course.CourseID = Enrollment.CourseID;
 
